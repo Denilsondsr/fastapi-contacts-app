bdcf8fe116e4b6d418915c177d8511279f221ebc
Denilson <denilson@denilson>
2024-11-13 17:31:13 -0300

    criando models, database e rodas

---
+++ app/models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+# Não esquecer de importar o base do database.py e apagar pois vai dar conflito
+from database import Base
+
+# Base = declarative_base()
+# Já esta Declarada no database.py
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+
+    contacts = relationship("Contact", back_populates="owner")
+
+class Contact(Base):
+    __tablename__ = 'contacts'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    phone = Column(String)
+    email = Column(String)
+    user_id = Column(Integer, ForeignKey('users.id'))
+
+    owner = relationship("User", back_populates="contacts")
+
